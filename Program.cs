7898a96a519bde9291ae51d6e4cc92d29bb789d7
MKEABB <mikaela_bk@hotmail.com>
2023-09-26 15:14:45 +0200

    parprogrammering

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,13 +11,51 @@ namespace bus
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 3213; i++)
+
+            /*
+            for (int i = 0; i < 32173; i++)
             {
                 EnKlassFörSig.DetVarEnGångEnRiddare();
                 EnKlassFörSig.OchEnTrollKarl();
                 EnKlassFörSig.OchDeGjordeMassaBus();
                 
             }
+            *
+            */
+            StreamReader reader = new StreamReader(@"C:\Users\MBB\source\repos\MKEABB\bus\neareuropecities.csv");
+
+            List<string> cities = new List<string>();
+
+            int stock = 0, oslo = 0;
+
+            using (reader)
+            {
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    foreach (var item in values)
+                    {
+
+                        cities.Add(item);
+                        if (item == "\"Stockholm\"")
+                        {
+
+                            stock++;
+                            Console.WriteLine(item);
+                        }
+                        else if (item == "\"Oslo\"")
+                        {
+                            oslo++;
+                            Console.WriteLine(item);
+                        }
+                    }
+                }
+                Console.WriteLine("Stockholm: " + stock);
+                Console.WriteLine("Oslo: " + oslo);
+                Console.ReadKey();
+            }
         }
+
     }
 }
