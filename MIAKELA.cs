3bf791237240e25899cd1cca5504b38c9f0f8f2b
MKEABB <mikaela_bk@hotmail.com>
2023-10-03 18:28:05 +0200

    LIten grej till mig själv bara

---
+++ MIAKELA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bus
+{
+    internal class MIAKELA
+    {
+        public void RemoveFromCallList(List<string[]> callHistory)
+        {
+            foreach (string[] item in callHistory)
+            {
+                Console.WriteLine(item);
+            }
+
+            string chosenIndex = string.Empty;
+            foreach (var numberInfo in callHistory)
+            {
+                string searchNumber = numberInfo[0];
+
+                if (chosenIndex == searchNumber)
+                {
+                    callHistory.RemoveAt(int.Parse(chosenIndex));
+                }
+            }
+        }
+    }
+}
